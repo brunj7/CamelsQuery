efc71425a9d15e7c382b0392bbfd91e62b2a15e6
Julien Brun <brun.julien@gmail.com>
2020-12-11 11:47:00 -0800

    cleaning hidden char

--- data-raw/chem_fields_drop.R
+++ data-raw/chem_fields_drop.R
@@ -1,5 +1,9 @@
 ## code to prepare `chem_fields_drop.csv` dataset goes here
 
 chem_fields_drop <- read.csv("inst/extdata/chem_fields_drop.csv")
-chem_fields_drop <- tidyr::separate(chem_fields_drop,field_drop, c("id", "fieldname"), sep = "-") %>% select(fieldname)
+chem_fields_drop <- tidyr::separate(chem_fields_drop,field_drop, c("id", "fieldname"), sep = "-") %>%
+  dplyr::select(fieldname) %>%
+  dplyr::mutate(fieldname = trimws(fieldname))
+
+# Write the file for the package
 usethis::use_data(chem_fields_drop, overwrite = TRUE)
