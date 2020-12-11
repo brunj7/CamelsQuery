2319be81f131290a579cfacaa8707f1d2cf20d6c
Julien Brun <brun.julien@gmail.com>
2020-12-11 11:17:10 -0800

    switching back to one column

--- data-raw/chem_fields_drop.R
+++ data-raw/chem_fields_drop.R
@@ -1,4 +1,5 @@
 ## code to prepare `chem_fields_drop.csv` dataset goes here
 
-chem_fields_drop <- readr::read_csv("inst/extdata/chem_fields_drop.csv")
+chem_fields_drop <- read.csv("inst/extdata/chem_fields_drop.csv")
+chem_fields_drop <- tidyr::separate(chem_fields_drop,field_drop, c("id", "fieldname"), sep = "-") %>% select(fieldname)
 usethis::use_data(chem_fields_drop, overwrite = TRUE)
